aab13f7484ad7e8c7ea3fb9e3844b5b06dcfbc14
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-26 15:26:22 +0530

    Add isDistinct function to CheckDistinct.js

---
+++ CheckDistinct.js
@@ -0,0 +1,15 @@
+function isUnique(currentelement,index,list) {
+    const elementOccurrences = element => element === currentelement;
+    let numberOfOccurrences = list.filter(elementOccurrences).length;
+    return numberOfOccurrences === 1;
+}
+function isDistinct(list) {
+    return list.every(isUnique)
+}
+
+function main() {
+    console.log(isDistinct([1,2,3]));
+    console.log(isDistinct([1,2,3,1]));
+}
+
+main();
