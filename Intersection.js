8c967150160716ae4ef2271eedcb8375d26a5993
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-27 12:12:24 +0530

    Modify intersection function in Intersection.js

--- Intersection.js
+++ Intersection.js
@@ -1,12 +1,9 @@
+function isElementPresent(element) {
+    return this.includes(element);
+}
+
 function intersection(list1, list2){
-    let result = [];
-    const isContain = function(element){
-        if(list2.includes(element)) {
-            result.push(element);
-        }
-    }
-    list1.forEach(isContain);
-    return result;
+    return list1.filter(isElementPresent,list2);
 }
 
 function main() {
