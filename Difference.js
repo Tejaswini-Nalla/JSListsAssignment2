261e150adb7e7a4a1bae971c161b966bf560bc3f
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-02-27 10:27:28 +0530

    Add difference function in Difference.js

--- Difference.js
+++ Difference.js
@@ -1,3 +1,17 @@
 function difference(list1, list2) {
-
+    let result = [];
+    const isContain = function(element){
+        if(!list2.includes(element)) {
+            result.push(element);
+        }
+    }
+    list1.forEach(isContain);
+    return result;
 }
+
+function main() {
+    console.log(difference([1,2,3],[1,2]));
+    console.log(difference([1,2,3,4],[1,2,5]));
+}
+
+main();
