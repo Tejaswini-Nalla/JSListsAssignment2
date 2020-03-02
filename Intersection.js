fed8cc7ce272083f3595727ea7f6221af497b08f
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-03-02 15:00:48 +0530

    Modify intersection function in Intersection.js

--- Intersection.js
+++ Intersection.js
@@ -1,13 +1,15 @@
-function isElementPresent(element) {
-    return this.includes(element);
-}
-
 function intersection(list1, list2){
-    return list1.filter(isElementPresent,list2);
+    let elements = list2;
+    return list1.filter((element) =>{
+                            if(elements.includes(element)) {
+                                elements.splice(elements.indexOf(element),1);
+                                return element;
+                            }
+                            });
 }
 
 function main() {
-    console.log(intersection([1,2,2,3],[2,3]));
+    console.log(intersection([1,2,2,3],[2,3,2,6]));
     console.log(intersection([1,2,3],[4,5,6]));
 }
 
